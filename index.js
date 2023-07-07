765edfaedfeadbe1b1179e536a9429069dea66b9
silverwind <me@silverwind.io>
2023-07-08 01:41:54 +0200

    small refactor

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ export function stringPlugin({match = /\.(svg|md)/i} = {}) {
     async load(id) {
       const path = id.split("?")[0];
       if (!match.test(path)) return null;
-      const data = await readFile(path, "utf8");
 
       // https://github.com/rollup/plugins/blob/master/packages/pluginutils/src/dataToEsm.ts
-      const str = JSON.stringify(data).replace(/[\u2028\u2029]/g,
+      const str = JSON.stringify(await readFile(path, "utf8")).replace(/[\u2028\u2029]/g,
         char => `\\u${`000${char.charCodeAt(0).toString(16)}`.slice(-4)}`
       );
       return `export default ${str};`;
