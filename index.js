69eff4c240b174272a2189cf98b6d4ff0a33e7e1
silverwind <me@silverwind.io>
2023-07-08 01:39:50 +0200

    remove quotes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ export function stringPlugin({match = /\.(svg|md)/i} = {}) {
       const data = await readFile(path, "utf8");
 
       // https://github.com/rollup/plugins/blob/master/packages/pluginutils/src/dataToEsm.ts
-      const str = (JSON.stringify(data)).replace(/[\u2028\u2029]/g,
+      const str = JSON.stringify(data).replace(/[\u2028\u2029]/g,
         char => `\\u${`000${char.charCodeAt(0).toString(16)}`.slice(-4)}`
       );
       return `export default ${str};`;
