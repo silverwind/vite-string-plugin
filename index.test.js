da8d2becb850c76e23c130127baaad2d161215e2
silverwind <me@silverwind.io>
2023-07-08 00:54:23 +0200

    add dynamic import test cases

--- index.test.js
+++ index.test.js
@@ -2,10 +2,14 @@ import {readFileSync} from "node:fs";
 import svg from "./fixtures/test.svg";
 import md from "./fixtures/test.md";
 
-test("svg", () => {
-  expect(svg).toEqual(readFileSync(new URL("fixtures/test.svg", import.meta.url), "utf8"));
+test("svg", async () => {
+  const expected = readFileSync(new URL("fixtures/test.svg", import.meta.url), "utf8");
+  expect(svg).toEqual(expected);
+  expect((await import("./fixtures/test.svg")).default).toEqual(expected);
 });
 
-test("md", () => {
-  expect(md).toEqual(readFileSync(new URL("fixtures/test.md", import.meta.url), "utf8"));
+test("md", async () => {
+  const expected = readFileSync(new URL("fixtures/test.md", import.meta.url), "utf8");
+  expect(md).toEqual(expected);
+  expect((await import("./fixtures/test.md")).default).toEqual(expected);
 });
