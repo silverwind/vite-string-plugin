30f238fb2669ec262390b8fe51f08297716d7077
silverwind <me@silverwind.io>
2023-07-08 02:01:05 +0200

    add pdf file test

--- index.test.js
+++ index.test.js
@@ -2,6 +2,7 @@ import {readFileSync} from "node:fs";
 import svg from "./fixtures/test.svg";
 import md from "./fixtures/test.md";
 import txt from "./fixtures/test.txt";
+import pdf from "./fixtures/test.pdf";
 
 test("svg", async () => {
   const expected = readFileSync(new URL("fixtures/test.svg", import.meta.url), "utf8");
@@ -19,4 +20,10 @@ test("txt", async () => {
   const expected = readFileSync(new URL("fixtures/test.txt", import.meta.url), "utf8");
   expect(txt).toEqual(expected);
   expect((await import("./fixtures/test.txt")).default).toEqual(expected);
+});
+
+test("pdf", async () => {
+  const expected = readFileSync(new URL("fixtures/test.pdf", import.meta.url), "utf8");
+  expect(pdf).toEqual(expected);
+  expect((await import("./fixtures/test.pdf")).default).toEqual(expected);
 });
