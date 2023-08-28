fd59f606fdbda4f2359d8b4942b21eae24707fd1
silverwind <me@silverwind.io>
2023-08-29 00:30:02 +0200

    refactor for size

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import {readFile} from "node:fs/promises";
 
-export function stringPlugin({match = /\.(svg|md)$/i} = {}) {
-  return {
-    name: "vite-string-plugin",
-    enforce: "pre",
-    load: async id => {
-      const path = id.split("?")[0];
-      if (!match.test(path)) return null;
-      return `export default ${JSON.stringify(await readFile(path, "utf8")).replace(
-        /[\u2028\u2029]/g, c => `\\u${`000${c.charCodeAt(0).toString(16)}`.slice(-4)}`
-      )};`;
-    }
-  };
-}
+export const stringPlugin = ({match = /\.(svg|md)$/i} = {}) => ({
+  name: "vite-string-plugin",
+  enforce: "pre",
+  async load(id) {
+    const path = id.split("?")[0];
+    if (!match.test(path)) return null;
+    return `export default ${JSON.stringify(await readFile(path, "utf8")).replace(
+      /[\u2028\u2029]/g, c => `\\u${`000${c.charCodeAt(0).toString(16)}`.slice(-4)}`
+    )};`;
+  }
+});
