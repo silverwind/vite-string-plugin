981d287d9af7c13315221e8b77f6386e91f22794
silverwind <me@silverwind.io>
2024-01-26 14:24:35 +0100

    update devDeps

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,6 @@ import {backendTest} from "vitest-config-silverwind";
 import {stringPlugin} from "./index.js";
 
 export default defineConfig({
-  test: backendTest,
+  test: backendTest({url: import.meta.url}),
   plugins: [stringPlugin({match: /\.(svg|md|txt|pdf)$/i})],
 });
