2067bb9bd3d099f51c515e2e8f0bc2dc207a2cba
silverwind <me@silverwind.io>
2024-12-03 05:31:24 +0100

    update deps, ignore vitest error

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from "vitest/config";
 import {backend} from "vitest-config-silverwind";
 import {stringPlugin} from "./index.ts";
 
+// @ts-expect-error: vite and vitest incompatible
 export default defineConfig(backend({
   url: import.meta.url,
   plugins: [stringPlugin({match: /\.(svg|md|txt|pdf)$/i})],
