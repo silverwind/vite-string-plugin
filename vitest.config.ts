dd0ed1a2886096026a4b9759d9f4725327a3ad40
silverwind <me@silverwind.io>
2025-02-14 18:50:22 +0100

    fix lint

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import {defineConfig} from "vitest/config";
 import {backend} from "vitest-config-silverwind";
 import {stringPlugin} from "./index.ts";
 
-// @ts-expect-error: vite and vitest incompatible
 export default defineConfig(backend({
   url: import.meta.url,
   plugins: [stringPlugin({match: /\.(svg|md|txt|pdf)$/i})],
